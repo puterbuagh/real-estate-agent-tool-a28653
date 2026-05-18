98f82a44379ebee7308f6987a83085b84a1e2b93
puterbaugh <126985461+puterbuagh@users.noreply.github.com>
2026-05-18 10:16:57 +0800

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+    
+    </head>
       <body>
         <AgentBrandingProvider>
           <PipelineProvider>
